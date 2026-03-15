682ec8025e7bb0cfb1e69c8d703c261c6012ef5c
Hirtik Malvi <hirtik.malvi@gmail.com>
2026-03-15 13:10:31 +0530

    feat: add Result.cs

---
+++ backend/CTPS.API/Common/Result.cs
@@ -0,0 +1,30 @@
+﻿namespace CTPS.API.Common
+{
+    public class Result<T>
+    {
+        public bool Success { get; set; }
+        public int StatusCode { get; set; }
+        public T? Data { get; set; }
+        public List<string>? Errors { get; set; }
+
+        public static Result<T> Ok(T data, int statusCode)
+        {
+            return new Result<T>
+            {
+                Success = true,
+                StatusCode = statusCode,
+                Data = data
+            };
+        }
+
+        public static Result<T> Fail(int statusCode = 400, List<string>? errors = null)
+        {
+            return new Result<T>
+            {
+                Success = false,
+                StatusCode = statusCode,
+                Errors = errors ?? new List<string>()
+            };
+        }
+    }
+}
